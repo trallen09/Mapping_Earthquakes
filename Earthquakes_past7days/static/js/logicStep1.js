9821c17a073940bd987c2faa4df27e789f992905
Thomas Allen <trallen09@yahoo.com>
2021-06-06 16:21:05 -0500

    13.6.1

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -23,9 +23,9 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -41,9 +41,8 @@ let torontoData = "https://raw.githubusercontent.com/trallen09/Mapping_Earthquak
 let torontoHoods = "https://raw.githubusercontent.com/trallen09/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function (data) {
-  console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
 });
